ae1d7a441c08e0aaf4e23702bb613e7ba7af25ce
Jared Peterson <hobbitcakes@gmail.com>
2017-12-05 21:05:44 -0600

    create hello world python-docx script

--- Python/automate_the_boring_stuff/createDocx.py
+++ Python/automate_the_boring_stuff/createDocx.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
+from docx import Document
 
-print('Hello')
+document = Document()
+
+paragraph = document.add_paragraph('Lorem ipsum dolor sit amet.')
+
+prior_paragraph = paragraph.insert_paragraph_before('Lorem ipsum')
+
+document.add_heading('The REAL meaning of the universe')
+document.add_heading('The role of dolphins', level=2)
+document.add_page_break()
+
+# Save the document
+document.save('hellowrld.docx')
+

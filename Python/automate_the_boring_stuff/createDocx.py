8cfb58a5470d3ae5d16e052c2fc4ca67c4d1ed66
Jared Peterson <hobbitcakes@gmail.com>
2017-12-15 15:04:55 -0600

    go stuff

--- Python/automate_the_boring_stuff/createDocx.py
+++ Python/automate_the_boring_stuff/createDocx.py
@@ -4,12 +4,12 @@ from docx import Document
 document = Document()
 
 paragraph = document.add_paragraph('Lorem ipsum dolor sit amet.')
-
 prior_paragraph = paragraph.insert_paragraph_before('Lorem ipsum')
-
 document.add_heading('The REAL meaning of the universe')
 document.add_heading('The role of dolphins', level=2)
 document.add_page_break()
+document.add_heading('The REAL meaning of the universe')
+document.add_heading('The role of dolphins', level=2)
 
 # Save the document
 document.save('hellowrld.docx')

1755e90d041a688040882840f51be30f82672e6d
Jared <hobbitcakes@gmail.com>
2018-01-22 14:20:34 +0000

    finished os_and_env_variables script

--- Python/linuxacademy/Exercises/os_and_env_variables.py
+++ Python/linuxacademy/Exercises/os_and_env_variables.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
 import os
-import pi from math
+import math
 
 # https://linuxacademy.com/cp/exercises/view/id/650/module/158
 
 # Print the first ten digits of Pi to the screen
 
 # Accpet an optional environment variable called DIGITS. If present, the script will print that many digits of Pi instead of 10.
+
+digits = int((os.getenv("DIGITS") or 10))
+
+pie = math.pi
+
+print("%.*f" % (digits, math.pi))

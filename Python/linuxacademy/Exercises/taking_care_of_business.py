70e4cbf2e0102035d3fec2af930c024fdbea5752
Jared <hobbitcakes@gmail.com>
2018-01-23 02:16:26 +0000

    taking care of business

---
+++ Python/linuxacademy/Exercises/taking_care_of_business.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# https://linuxacademy.com/cp/exercises/view/id/653/module/158
+
+# Take port_number as only argument
+
+# call lsof to determine if there is a process listening on that port
+    # If there is, kill the process and inform the user
+    # If there is no process, prkint that there was noprocess running on that port
+
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description='Kill any process listening on PORT')
+parser.add_argument('--port', help='port to free up')
+args = parser.parse_args()
+
+try:
+    port_taken = subprocess.check_output(['lsof', '-n', ('-i4TCP:%s' % args.port)], stderr=subprocess.STDOUT)
+    proc = port_taken.split()
+except subprocess.CalledProcessError:
+    print("Process not taken")
+else:
+    subprocess.check_output(['kill', '-9', proc[10]])
+    print("Killed %s" % proc[1])
+

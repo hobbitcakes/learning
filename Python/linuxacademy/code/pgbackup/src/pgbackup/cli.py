10ef0368d0558922d08a0dedfdb31e47541840d1
Jared <hobbitcakes@gmail.com>
2018-01-30 02:33:20 +0000

    wrote a few tests

---
+++ Python/linuxacademy/code/pgbackup/src/pgbackup/cli.py
@@ -0,0 +1,15 @@
+import argparse
+
+class DriverAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        driver, destination = values
+        namespace.driver = driver.lower()
+        namespace.destination = destination
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="""
+    Backup PostgreSQL databases locallay or to AWS S3
+    """)
+    parser.add_argument('url', help='URL of database to backup')
+    parser.add_argument('--driver', help='how & where to store backup', nargs=2, action=DriverAction, required=True)
+    return parser

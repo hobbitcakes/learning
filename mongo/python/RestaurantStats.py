21098b5e9428751f600f8f1acdb95ae3c2b89318
Jared Peterson <hobbitcakes@gmail.com>
2018-03-15 21:07:33 -0500

    first role

--- mongo/python/RestaurantStats.py
+++ mongo/python/RestaurantStats.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 from pymongo import MongoClient
 import pprint
+import pandas
 
 # Create a MongoClient to the running mongo instance
 client = MongoClient('mongodb://localhost:27017/')
 
 # get the test database
 db = client['test']
-restaurant = db['restaurant']
-pprint.pprint(restaurant.find_one())
+restaurants = db['restaurants']
+pprint.pprint(restaurants.find_one())
+
+cursor = db['restaurants'].find({})
+dataframe = pandas.DataFrame(list(cursor))
+
+print(dataframe)

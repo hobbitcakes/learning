559eb200e00a9023c9e417a0df5239015dde4fcd
Jared Peterson <hobbitcakes@gmail.com>
2018-12-23 12:24:43 -0600

    admin interface

--- Python/web/django/tictactoe/gameplay/models.py
+++ Python/web/django/tictactoe/gameplay/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+GAME_STATUS_CHOICES = (
+    ('F', 'First Player to Move'),
+    ('S', 'Second Player to Move'),
+    ('W', 'First Player Wins'),
+    ('L', 'Second Player Wins'),
+    ('D', 'Draw')
+)
+
 class Game(models.Model):
     first_player = models.ForeignKey(User, on_delete=models.CASCADE, related_name="games_first_player")
     second_player = models.ForeignKey(User, on_delete=models.CASCADE, related_name="games_second_player")
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=1, default='F')
+    status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
+
+    def __str__(self):
+        return "%s vs %s" % (self.first_player, self.second_player)
     
 
 class Move(models.Model):

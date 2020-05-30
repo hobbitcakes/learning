faa7fd890934440e6aede3c59c34d7155dff57db
Jared Peterson <hobbitcakes@gmail.com>
2020-05-29 22:47:37 -0500

    Better code

---
+++ golang/chrome/incognito.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+)
+
+const CHROME_BINARY = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+
+func openChrome() {
+	cmd := exec.Command(CHROME_BINARY, "-incognito")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mess := "Waiting on " + CHROME_BINARY
+	log.Printf(mess)
+	err = cmd.Wait()
+	log.Printf("Command Finished")
+
+}
+
+func sleep() {
+	cmd := exec.Command("/bin/sleep", "5")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Waiting for command to finish...")
+	err = cmd.Wait()
+	log.Printf("Command finished")
+
+}
+
+func main() {
+	openChrome()
+	sleep()
+}

e5cc4627052e24bbf2ab28d2d6384becc2b9e8a2
hobbitcakes <hobbitcakes@gmail.com>
2021-12-03 22:34:54 -0600

    Removed some old libraries

--- leetcode/FirstUniqueCharacterInAString/src/main/java/FirstUniqueCharacterInAString.java
+++ leetcode/FirstUniqueCharacterInAString/src/main/java/FirstUniqueCharacterInAString.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class FirstUniqueCharacterInAString {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FirstUniqueCharacterInAString {
+        public int firstUniqChar(String s) {
+            Map d = new HashMap<Character, ArrayList<Integer>>();
+
+            for(int i = 0; i < s.length(); i++) {
+                char key = s.charAt(i);
+                d.putIfAbsent(key, new ArrayList<Integer>());
+                ArrayList<Integer> l = d.get(key);
+
+            }
+
+
+            for(Character c : d.keySet()) {
+                if(d.get(c).size() == 1) {
+                    return d.get(c).get(0);
+                }
+            }
+
+            return -1;
+        }
 }
